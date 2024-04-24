043e6f4e6851b06e0f0609afa5bc0d37faa5137a
Bagjax <rifkisubagja23@gmail.com>
2024-04-24 13:02:15 +0700

    update Crud Phonebook

--- Controllers/PhoneController.cs
+++ Controllers/PhoneController.cs
@@ -26,6 +26,56 @@ namespace menyala.Controllers
             return View("Phones", phones); // Menggunakan nama view "Phones" secara eksplisit
         }
 
+        [Authorize]
+        [HttpPost]
+        public IActionResult Create(Phone phone)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Phones.Add(phone);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Phones));
+            }
+            return View(phone); // Jika model tidak valid, kembali ke form create dengan data yang diinput
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Update(int id, Phone updatedPhone)
+        {
+            var phone = _context.Phones.FirstOrDefault(p => p.Id == id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                phone.name = updatedPhone.name;
+                phone.pic = updatedPhone.pic;
+                phone.nomor = updatedPhone.nomor;
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Phones));
+            }
+
+            return View(phone); // Jika model tidak valid, kembali ke form update dengan data yang diinput
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var phone = _context.Phones.FirstOrDefault(p => p.Id == id);
+            if (phone == null)
+            {
+                return NotFound();
+            }
+
+            _context.Phones.Remove(phone);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Phones));
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
