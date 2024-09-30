c0af6dbeb577a010789cb789a9e85dd75d2f6e5b
Shafa <akushafaa27@gmail.com>
2024-09-30 14:45:58 +0700

    statusupdate

--- wwwroot/assets/js/apps.js
+++ wwwroot/assets/js/apps.js
@@ -1,26 +1,35 @@
 function createDetailPanel(reading) {
   var photo = '/images/favicon.png';
+  console.log('reading:', reading); // Log reading untuk debugging
 
-  console.log('reading.deviceStatus:', reading.deviceStatus);
+  // Default status OFFLINE
+  var statusOffline = '<small class="mdi mdi-checkbox-blank-circle text-danger"></small> OFFLINE';
 
-  var status_offline = '<small class="mdi mdi-checkbox-blank-circle text-danger"></small> OFFLINE';
-
-  if (reading.deviceStatus === 'online') {
-    status_offline = '<small class="mdi mdi-checkbox-blank-circle text-success"></small> ONLINE';
-    console.log('Status is online');
+  // Cek status dari setiap jenis pembacaan
+  if (reading.arrLastReading && reading.arrLastReading.deviceStatus.toLowerCase() === 'online') {
+    statusOffline = '<small class="mdi mdi-checkbox-blank-circle text-success"></small> ONLINE';
+    console.log('Status from ARR is online');
+  } else if (reading.awlrLastReading && reading.awlrLastReading.deviceStatus.toLowerCase() === 'online') {
+    statusOffline = '<small class="mdi mdi-checkbox-blank-circle text-success"></small> ONLINE';
+    console.log('Status from AWLR is online');
+  } else if (reading.awsLastReading && reading.awsLastReading.deviceStatus.toLowerCase() === 'online') {
+    statusOffline = '<small class="mdi mdi-checkbox-blank-circle text-success"></small> ONLINE';
+    console.log('Status from AWS is online');
+  } else if (reading.awlrArrLastReading && reading.awlrArrLastReading.deviceStatus.toLowerCase() === 'online') {
+    statusOffline = '<small class="mdi mdi-checkbox-blank-circle text-success"></small> ONLINE';
+    console.log('Status from AWLR ARR is online');
   } else {
-    console.log('Status is offline');
+    console.log('All statuses are offline');
   }
 
   var panelContent = '<table class="table mb-2 font-12"><tbody>';
-
   panelContent += '<tr>';
   panelContent += `<td class="px-0 py-2" colspan="3">
         <div class="d-flex align-items-start">
-            <img class="me-2 rounded-3" src="/assets/img/PUPR.jpeg" width="45" height="45" alt="${reading.deviceId}">
+            <img class="me-2 rounded-3" src="/assets/img/PUPR.jpg" width="45" height="45" alt="${reading.deviceId}">
             <div class="w-100">
-                <h5 class="mt-0 mb-1 fw-semibold font-12">${reading.slug}</h5>
-                ${status_offline}
+                <h5 class="mt-0 mb-1 fw-semibold font-12">${reading.name}</h5>
+                ${statusOffline}
             </div>
         </div>
     </td>`;
@@ -45,7 +54,6 @@ function createDetailPanel(reading) {
   panelContent += '</tr>';
 
   panelContent += '</tbody></table>';
-
   panelContent += `<div class="text-end"><a href="/Home/Detail?code=${reading.code}" target="_blank">Lihat Detail <i class="mdi mdi-arrow-right"></i></a></div>`;
 
   return panelContent;
@@ -58,12 +66,6 @@ function initMap() {
     center: initialLocation,
     mapTypeId: google.maps.MapTypeId.SATELLITE 
   });
-
-  // // Menambahkan marker awal
-  // var marker = new google.maps.Marker({
-  //   position: initialLocation,
-  //   map: map,
-  // });
 
   // Panggil fungsi untuk mendapatkan data stasiun dan menambahkan marker
   GetDataStation(map);
@@ -93,6 +95,8 @@ function initMap() {
   map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legendDiv);
 }
 
+let currentInfoWindow = null;
+
 async function GetDataStation(map) {
   try {
     const response = await fetch('/Api/GetStationAll');
@@ -101,42 +105,49 @@ async function GetDataStation(map) {
       var datas = JSON.parse(listStation);
       let stations = datas.data;
 
+      console.log('List of Stations:', stations); // Log untuk memeriksa isi data
+
       // Loop melalui data stasiun untuk menambahkan marker ke peta
       stations.forEach(station => {
+        // Pastikan latitude dan longitude ada
+        if (!station.latitude || !station.longitude) {
+          console.warn(`Station ${station.slug} is missing latitude or longitude`);
+          return; // Skip marker creation
+        }
+
         var position = { lat: station.latitude, lng: station.longitude };
-        //Marker sesuai gambar
+        
+        // Marker sesuai gambar
         var iconUrl = '';
         switch (station.stationType) {
           case 'ARR':
-            iconUrl = '/assets/img/curah.png'; // Ganti dengan URL ikon PDA/AWLR
+            iconUrl = '/assets/img/curah.png'; // Ganti dengan URL ikon PCH/ARR
             break;
           case 'AWLR':
-            iconUrl = '/assets/img/duga.png'; // Ganti dengan URL ikon PCH/ARR
+            iconUrl = '/assets/img/duga.png'; // Ganti dengan URL ikon PDA/AWLR
             break;
           case 'AWS':
-            iconUrl = '/assets/img/klimatologi.png'; // Ganti dengan URL ikon PDA & PCH
+            iconUrl = '/assets/img/klimatologi.png'; // Ganti dengan URL ikon AWS
             break;
           case 'AWLR_ARR':
-            iconUrl = '/assets/img/awlr.png'; // Ganti dengan URL ikon AWS
+            iconUrl = '/assets/img/awlr.png'; // Ganti dengan URL ikon PDA & PCH
             break;
           default:
             iconUrl = '/assets/img/default.png'; // Ikon default jika tipe tidak dikenali
             break;
         }
+        
+        console.log('Icon URL:', iconUrl); // Log untuk memeriksa URL ikon
+
         var marker = new google.maps.Marker({
           position: position,
           map: map,
-          icon:{
+          icon: {
             url: iconUrl,
-            scaledSize: new google.maps.Size(30,35),
+            scaledSize: new google.maps.Size(30, 35),
           },
           title: station.slug, // Memberi nama pada marker
         });
-        // var marker = new google.maps.Marker({
-        //   position: position,
-        //   map: map,
-        //   title: station.name, // Memberi nama pada marker
-        // });
 
         // Buat infoWindow menggunakan createDetailPanel
         var infoWindow = new google.maps.InfoWindow({
@@ -144,7 +155,12 @@ async function GetDataStation(map) {
         });
 
         marker.addListener('click', function() {
+          // Tutup InfoWindow sebelumnya jika ada 
+          if (currentInfoWindow) {
+            currentInfoWindow.close();
+          }
           infoWindow.open(map, marker);
+          currentInfoWindow = infoWindow;
         });
       });
     } else {
